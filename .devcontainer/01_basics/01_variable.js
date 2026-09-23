083572d40d1e76b7e4d34e87da3540fa829916e1
Raushan Kumar <raushankumar9508434926@gmail.com>
2026-09-23 14:27:58 +0000

    variable done

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=1234
+let accountName="Raushan"
+var accountEmail="rau@gmail.com"
+city="Hyd"
+let accounState;
+
+
+// accountId=2;
+accountEmail="R@gmail"
+accountName="Rag"
+city="Jarnataka"
+console.table([accountId,accountEmail,accountName,city,accounState])
+
+console.log(accountId)
