c5b7ec4824b1e6ee3c640a6d5d28e84870d6a036
Raushan Kumar <raushankumar9508434926@gmail.com>
2026-09-24 11:18:31 +0000

    string

---
+++ .devcontainer/01_basics/string.js
@@ -0,0 +1,24 @@
+const name="Raghu";
+const repo = "js-repo";
+
+
+console.log("my name is :"+name+" My repository name is :"+repo);
+console.log(`hello My name is : ${name} and My repo name is ${repo}`);
+
+console.log("=========================");
+const name1=new String('Raghavendra');
+console.log(name1[0]);
+console.log(name1.toUpperCase());
+console.log(name1.toLowerCase());
+console.log(name1.length);
+console.log(name1.charAt(2));
+console.log(name1.indexOf('a'));
+console.log(name1.substring(0,4));//here we can not take negative value
+console.log(name1.slice(0,4));
+console.log(name1.slice(-8,6));
+console.log("=============================");
+const place="     Ameerpet    ";
+console.log(place.length);
+console.log(place.trim());
+console.log(place.length);
+console.log(name1.replace('R','A'));
