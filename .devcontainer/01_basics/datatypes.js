0a3852fcce02b00c1d906d22212d90ceb06c765d
Raushan Kumar <raushankumar9508434926@gmail.com>
2026-09-24 07:06:16 +0000

    primitiveAndnonPrimitive

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,58 @@
+//there are 2 types mainaly
+//1.premitive datatype
+   //-String
+   let string = "Raushan";
+   console.log(string);
+   console.log(typeof string);
+    console.log("**********************************");
+   
+   //number
+   let num = 123;
+   console.log(num);
+   console.log(typeof num);
+    console.log("**********************************");
+   
+   //boolean
+   let isTrue=true;
+   console.log(isTrue);
+   console.log(typeof isTrue);
+    console.log("**********************************");
+   
+   //undefined
+   let number;
+   console.log(number);
+   console.log(typeof number);
+    console.log("**********************************");
+   
+   //null
+   let num1=null;
+   console.log(num1);
+    console.log("**********************************");
+   
+//2.non-premitive datatype(reference dtatype)
+       //array
+       let arr=[1,3,"Raushan"];
+       console.log(arr);
+       console.log(typeof arr);
+       
+       console.log("**********************************");
+       
+
+       //object
+       let myObj={
+        name:"Ram",
+        email:"r@123",
+        number:1020304050
+       }
+       console.log(myObj);
+       console.log(typeof myObj);
+
+        console.log("**********************************");
+
+       //function
+       const myFun = function(){
+        console.log("hello and welcome JS");
+       }
+       myFun();
+       console.log(typeof myFun);
+        console.log("**********************************");
