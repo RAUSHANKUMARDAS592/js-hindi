00aca590a90149c70ed4217fcc90f955c3054981
Raushan Kumar <raushankumar9508434926@gmail.com>
2026-09-24 13:57:12 +0000

    02-array

---
+++ .devcontainer/01_basics/02-array.js
@@ -0,0 +1,10 @@
+let arr1 = ["Raushan","Raghu"];
+let arr2 = ["Allen","Rohit"];
+
+let add = [...arr1, ...arr2];//fore concatinate 
+console.log(add);
+
+const arr3 = [12,23,[1,3,4],2,[3,[4,5]]];
+console.log(arr3.flat(Infinity));//used to make one single array
+
+
