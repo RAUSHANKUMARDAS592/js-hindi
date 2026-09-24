ffc56080a887c174bf3bfd277b2917b7f31b5fd9
Raushan Kumar <raushankumar9508434926@gmail.com>
2026-09-24 06:00:40 +0000

    changed

--- .devcontainer/01_basics/coonversionandoperation.js
+++ .devcontainer/01_basics/coonversionandoperation.js
@@ -5,3 +5,35 @@ console.log(boolean);
 let number=123;
 let StringNumber = String(number);
 console.log(typeof(StringNumber));
+
+console.log("*************************************************");
+let num1=2;
+let num2=3;
+console.log(num1+num2);
+console.log("***********************");
+
+let num=12;
+let negativeNum = -num;
+console.log(negativeNum);
+
+console.log("***************************");
+console.log(1+2);
+console.log("1"+2);
+console.log("1"+2+3);
+console.log(1+2+"4");
+
+console.log("========================");
+let a = 5;
+let b=++a;
+console.log(a);
+console.log(b);
+
+console.log("-------------------------");
+
+let incA = 5;
+let incB=incA++;
+console.log(incA);
+console.log(incB);
+
+
+
