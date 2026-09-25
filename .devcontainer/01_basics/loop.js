ff43b8c3232469351872b4ac726398940c2f4933
Raushan Kumar <raushankumar9508434926@gmail.com>
2026-09-25 11:15:30 +0000

    loops

---
+++ .devcontainer/01_basics/loop.js
@@ -0,0 +1,37 @@
+// let array=[1,2,3,4,5];
+
+// for (let i = 0;i < array.length;i++) {
+//     const element = array[i];
+//if(i ==5){
+    //console.log("break");
+    //break;
+    //continue;
+//}
+//console.log(element);
+    
+// }
+
+// let num=0;
+// while(num<=10){
+//     console.log(num);
+//     num++;
+    
+// }
+// let array=[1,3,5,4,7,7];
+// let i=0;
+
+// while(i<array.length){
+//     console.log(array[i]);
+//     i++;
+    
+// }
+
+//do while loop
+let score=1;
+
+do{
+    console.log(score);
+    score++;
+    
+
+}while(score<=10);
